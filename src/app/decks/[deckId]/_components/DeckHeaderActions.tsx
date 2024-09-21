39d3de9897fcd831154204a594a8272daa0043e3
Ziya Fenn <ziya.fenn@gmail.com>
2024-09-21 17:29:08 +0200

    deck interaction button animate

--- src/app/decks/[deckId]/_components/DeckHeaderActions.tsx
+++ src/app/decks/[deckId]/_components/DeckHeaderActions.tsx
@@ -2,7 +2,8 @@
 
 import { copyDeck, likeDeck } from "@/actions/deck.action";
 import { Button } from "@/components/ui/button";
-import { Heart } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Heart, ThumbsUp } from "lucide-react";
 import { useState } from "react";
 
 type DeckHeaderProps = {
@@ -23,11 +24,16 @@ export function DeckHeaderActions({
   authorId,
 }: DeckHeaderProps) {
   const [isLiked, setIsLiked] = useState(didUserLike);
+  const [hasPressedCopy, setHasPressedCopy] = useState(false);
+  const [hasPressedLike, setHasPressedLike] = useState(false);
 
   async function onCopy() {
     await navigator.clipboard.writeText(deckCode);
     const cookies = document.cookie;
     const hasUserCopiedDeck = checkACookieExists(cookies);
+
+    setHasPressedCopy(true);
+    setTimeout(() => setHasPressedCopy(false), 2000); // Revert back
 
     if (!hasUserCopiedDeck) {
       document.cookie = `copy=true;path=/decks/${deckId}`;
@@ -36,17 +42,44 @@ export function DeckHeaderActions({
   }
 
   async function onLike() {
+    setHasPressedLike(true);
+    setTimeout(() => setHasPressedLike(false), 2000); // Revert back
+
     setIsLiked(true);
     await likeDeck({ author_id: authorId, deck_id: deckId });
   }
 
   return (
     <div className="flex items-center gap-4">
-      <Button type="button" onClick={onLike} size="icon" disabled={isLiked}>
-        <Heart />
+      <Button
+        type="button"
+        variant="secondary"
+        onClick={onLike}
+        disabled={isLiked}
+        className={cn(
+          "transition duration-75",
+          hasPressedLike && "bg-green-100",
+        )}
+      >
+        <Heart className={cn(hasPressedLike && "animate-bounce")} />
       </Button>
-      <Button type="button" onClick={onCopy}>
-        Copy Deck
+      <Button
+        type="button"
+        variant="secondary"
+        onClick={onCopy}
+        className={cn(
+          "transition duration-75",
+          hasPressedCopy && "bg-green-100",
+        )}
+      >
+        {hasPressedCopy ? (
+          <span className="flex gap-2">
+            <ThumbsUp className="size-4" />
+            Copied
+          </span>
+        ) : (
+          "Copy Deck"
+        )}
       </Button>
     </div>
   );
